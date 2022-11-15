c444f2b7a57c472270a6eae127201b8a048f6a25
Jose M. Escrich <josemaria@escrich-sanchez.com>
2022-11-15 22:16:04 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -61,7 +61,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "Escrich, Phoenix(none, default config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Escrich, Phoenix" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
